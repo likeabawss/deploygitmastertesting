84090b75630718e1d92aef2cc471a600a3f52943
admin.mbarrett <admin.mbarrett@powerfarming.co.nz>
2019-01-02 08:23:39 +1300

    test only

--- deploytest.ps1
+++ deploytest.ps1
@@ -1,3 +1,2 @@
-. C:\users\mbarrett\Documents\Dev\AppInfraCodeCommon\GitDeployMaster.ps1
+&C:\users\mbarrett\Documents\Dev\AppInfraCodeCommon\GitDeployMaster.ps1 -readOnlyMasterRepoPath "C:\users\mbarrett\Documents\dev\deploygitmastertestingremote"
 
-$rslty = DeployGitMaster($pwd.path)
